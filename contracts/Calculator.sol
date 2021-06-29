da326645732f3234572076d0f7f058ffd20673d0
jwdev66 <jwdev66@gmail.com>
2021-06-29 03:13:16 -0500

    added add calling count

--- contracts/Calculator.sol
+++ contracts/Calculator.sol
@@ -5,10 +5,17 @@ pragma solidity ^0.7.3;
 contract Calculator {
     uint256 public calculateResult;
     address public user;
+    uint256 public calculatorAddCount;
     
     event Add(uint256 a, uint256 b);
     
+    constructor() {
+        calculatorAddCount = 0;
+    }
+
     function add(uint256 a, uint256 b) public returns (uint256) {
+        calculatorAddCount++;
+
         calculateResult = a + b;
         assert(calculateResult >= a);
         
--- contracts/Machine.sol
+++ contracts/Machine.sol
@@ -5,12 +5,14 @@ pragma solidity ^0.7.3;
 contract Machine {    
     uint256 public calculateResult;
     address public user;
-  
+    uint256 public machineAddCount;
+
     event AddedValuesByDelegateCall(uint256 a, uint256 b, bool success);
     event AddedValuesByCall(uint256 a, uint256 b, bool success);
     
     constructor() {
         calculateResult = 0;
+        machineAddCount = 0;
     }
     
     function addValuesWithDelegateCall(address calculator, uint256 a, uint256 b) public returns (uint256) {
